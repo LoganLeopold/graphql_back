d23de585c2a1c29f9f27722748916c312108735f
Logan Leopold <loganleopld@me.com>
2021-03-26 15:51:25 -0400

    at least have minimal error handling on create

--- controllers/actorControl.js
+++ controllers/actorControl.js
@@ -4,16 +4,19 @@ module.exports = {
 
     list: async (req,res) => {
         let actors = await Actor.find({})
-        // console.log(actors)
         res.send(actors)
     },
 
-    create: async (req,res) => {
-        let actor = await Actor.create({
-            Name: req.body.name,
-            Movies: []
-        })
-        res.send(actor)
+    create: async (req,res, next) => {
+        try {
+            let actor = await Actor.create({
+                Name: req.body.name,
+                Movies: []
+            })
+            res.send(actor)
+        } catch (err) {
+            console.log(err)
+        }
     }
 
 }
