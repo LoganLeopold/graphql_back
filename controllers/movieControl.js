cec9911d01d842ec89ad400b6d9313e95221e4fd
Logan Leopold <loganleopld@me.com>
2021-03-26 16:56:38 -0400

    digging into mongoose docs to dynamically save from multiple array values

--- controllers/movieControl.js
+++ controllers/movieControl.js
@@ -2,9 +2,50 @@ const Movie = require("../models/movie")
 
 module.exports = {
 
-    list: async(req,res) => {
+    list: async (req,res) => {
         let movies = await Movie.find({})
         res.send(movies)
+    },
+
+    create: async (req, res) => {
+
+        //process req.body as needed
+
+        const { 
+            name, 
+            actors, 
+            platforms, 
+            tom_pub, 
+            tom_crit, 
+            genres
+        } = req.body;
+
+        let actorSplits = actors.trim().split(",")
+        let platformSplits = genres.trim().split(",")
+        let genreSplits = genres.trim().split(",")
+
+        try {   
+
+            //Create Movie
+            let movie = await Movie.create({
+                Name: name,
+                Actors: [],
+                Platforms: [],
+                TomatoPublic: tom_pub,
+                TomatoCritic: tom_crit,
+                Genres: [],
+            })
+
+            actorSplits.forEach( actor => {
+
+                let name = actor.trim()
+                let actorDoc = await Actor.findOneAndUpdate({name: name})
+
+            })
+
+        } catch (err) {
+
+        }
     }
 
 }
