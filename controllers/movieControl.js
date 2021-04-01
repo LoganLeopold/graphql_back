faa825e76c338199ea2260faf492800953a01633
Logan Leopold <loganleopld@me.com>
2021-04-01 13:13:58 -0400

    succesful movie/create without upserts

--- controllers/movieControl.js
+++ controllers/movieControl.js
@@ -9,8 +9,6 @@ module.exports = {
 
     create: async (req, res) => {
 
-        //process req.body as needed
-
         const { 
             name, 
             director,
@@ -19,35 +17,21 @@ module.exports = {
             tom_pub, 
             tom_crit, 
             genres
-        } = req.body;
+        } = await req.body;
 
-        console.log(name)
-
-        // let actorSplits = actors.trim().split(",")
-        // let platformSplits = genres.trim().split(",")
-        // let genreSplits = genres.trim().split(",")
-
-        console.log(req)
         try {   
 
             //Create Movie
             let movie = await Movie.create({
                 Name: name,
-                Director: director,
+                // Director: director,
                 Actors: [],
                 Platforms: [],
-                TomatoPublic: pub,
-                TomatoCritic: crit,
+                TomatoPublic: parseInt(tom_pub),
+                TomatoCritic: parseInt(tom_crit),
                 Genres: [],
             })
 
-            
-            // actorSplits.forEach( actor => {
-
-            //     let name = actor.trim()
-            //     let actorDoc = await Actor.findOneAndUpdate({name: name})
-
-            // })
             res.send(movie)
 
         } catch (err) {
