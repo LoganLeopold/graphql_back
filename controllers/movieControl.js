e8d8d884342db13e8c9a554941edc62a03b63743
Logan Leopold <loganleopld@me.com>
2021-04-01 14:18:43 -0400

    check ln 31 note @ moviecontrol

--- controllers/movieControl.js
+++ controllers/movieControl.js
@@ -1,4 +1,7 @@
 const Movie = require("../models/movie")
+const Director = require("../models/director")
+const Platform = require("../models/platform")
+const Actor = require("../models/actor")
 
 module.exports = {
 
@@ -17,22 +20,49 @@ module.exports = {
             tom_pub, 
             tom_crit, 
             genres
-        } = await req.body;
+        } = req.body;
 
         try {   
 
             //Create Movie
-            let movie = await Movie.create({
-                Name: name,
-                // Director: director,
-                Actors: [],
-                Platforms: [],
-                TomatoPublic: parseInt(tom_pub),
-                TomatoCritic: parseInt(tom_crit),
-                Genres: [],
-            })
+            var movie = null;
+            let check = await Movie.findOne({Name: name})
 
-            res.send(movie)
+            // Thinking about creating all of the document objects first so I have accurate IDs and then inserting them/creating them dynamically using objectIDs
+            
+            if (!check) {
+
+                let genresInsert = genres.split(',').map( genre =>  genre.trim() )
+
+                let actorsInsert = actors.split(',').map( actor => actor.trim() )
+
+                let platformsInsert = platforms.split(',').map( platform => platform.trim() )
+
+                var movie = await Movie.create({
+                    Name: name,
+                    // Director: director,
+                    Actors: [],
+                    Platforms: [],
+                    TomatoPublic: parseInt(tom_pub),
+                    TomatoCritic: parseInt(tom_crit),
+                    Genres: genresInsert,
+                }) 
+
+                let directorUpsert = await Director.findOneAndUpdate(
+                    {Name: director},
+                    {$push: {Movies: movie._id}},
+                    {$upsert: true})
+
+                actorsInsert.forEach( actor => {
+                    let actor = await Actor.findOneAndUpdate(
+                        {Name: actor}, 
+                        {$push: {Movies: movie._id}},
+                        {$upsert: true})
+                })
+
+
+
+            }
 
         } catch (err) {
             console.log(err)
