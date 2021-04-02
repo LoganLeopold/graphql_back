7f5a577084d743c622499b4434d573d3321e129e
Logan Leopold <loganleopld@me.com>
2021-04-02 15:15:02 -0400

    got some ~hopefully~ solid async await array map action for objectIDs

--- controllers/movieControl.js
+++ controllers/movieControl.js
@@ -29,6 +29,7 @@ module.exports = {
 
             Movie to DB Pseudocode
 
+            1. Does movie exist? 
             2. Get values from req.body (above)
             3. Establish IDs for use 
                 -explicit for movieID + directorID
@@ -38,34 +39,37 @@ module.exports = {
             */ 
            
            // 1
-           let movieID = mongoose.Types.ObjectId()
-           let directorID = mongoose.Types.ObjectId()
-           let actorIDs = []
-           let platformIDs = []
-           
-           let insertDir = {
-               _id: directorID,
-               Name: director
-           }
 
-           
+           let movie = await Movie.findOne({Name: name})
+
+           let movieID = !movie ? mongoose.Types.ObjectId() : movie._id
+
+           // 2
+
+           let directorIns = await Director.findOne(({
+               Name: director.trim()
+           }))
+
+           let directorID = !directorIns ? mongoose.Types.ObjectId() : directorIns._id
+
            let actorsArr = actors.split(',').map( actor => {
-            // use this info for findoneandupdate with upsert - returned id will get pushed into actorIDs
-                // let actObj = {
-                //     Name: actor.trim(),
-                //     Movies: [movieID]
-                // }
-                // actObj.Name = actor.trim()
-                // actObj.Movies = [movieID]
-                // return actObj 
-                let actor = await Actor.findOneAndUpdate(
-                    {Name: actor.trim()}, 
-                    {$push: {Movies: movie._id}},
-                    {$upsert: true}
-                )
-
-                actorIDs.push(actor._id)
+            let actorIns = await Actor.findOneAndUpdate(
+                {Name: actor.trim()}, 
+                {$push: {Movies: movieID}},
+                {$upsert: true}
+            )
+            return actorIns._id
            })
+
+           let platformArr = platforms.split(',').map( platform => {
+               let platformIns = await Platform.findOneAndUpdate(
+                   {Name: platform.trim()},
+                   {$push: {Movies: movieID}},
+                   {$upsert: true}
+               )
+               return platformIns._id
+           })
+
 
             
 
@@ -73,45 +77,45 @@ module.exports = {
             // Below here is original try 
 
             //Create Movie
-            var movie = null;
-            let check = await Movie.findOne({Name: name})
+                // var movie = null;
+                // let check = await Movie.findOne({Name: name})
 
             // Thinking about creating all of the document objects first so I have accurate IDs and then inserting them/creating them dynamically using objectIDs
 
-            if (!check) {
+                // if (!check) {
 
-                let genresInsert = genres.split(',').map( genre =>  genre.trim() )
+                //     let genresInsert = genres.split(',').map( genre =>  genre.trim() )
 
-                let actorsInsert = actors.split(',').map( actor => actor.trim() )
+                //     let actorsInsert = actors.split(',').map( actor => actor.trim() )
 
-                let platformsInsert = platforms.split(',').map( platform => platform.trim() )
+                //     let platformsInsert = platforms.split(',').map( platform => platform.trim() )
 
-                var movie = await Movie.create({
-                    Name: name,
-                    // Director: director,
-                    Actors: [],
-                    Platforms: [],
-                    TomatoPublic: parseInt(tom_pub),
-                    TomatoCritic: parseInt(tom_crit),
-                    Genres: genresInsert,
-                }) 
+                //     var movie = await Movie.create({
+                //         Name: name,
+                //         // Director: director,
+                //         Actors: [],
+                //         Platforms: [],
+                //         TomatoPublic: parseInt(tom_pub),
+                //         TomatoCritic: parseInt(tom_crit),
+                //         Genres: genresInsert,
+                //     }) 
 
-                let directorUpsert = await Director.findOneAndUpdate(
-                    {Name: director},
-                    {$push: {Movies: movie._id}},
-                    {$upsert: true})
+                //     let directorUpsert = await Director.findOneAndUpdate(
+                //         {Name: director},
+                //         {$push: {Movies: movie._id}},
+                //         {$upsert: true})
 
-                actorsInsert.forEach( actor => {
-                    let actor = await Actor.findOneAndUpdate(
-                        {Name: actor}, 
-                        {$push: {Movies: movie._id}},
-                        {$upsert: true}
-                    )
-                })
+                //     actorsInsert.forEach( actor => {
+                //         let actor = await Actor.findOneAndUpdate(
+                //             {Name: actor}, 
+                //             {$push: {Movies: movie._id}},
+                //             {$upsert: true}
+                //         )
+                //     })
 
 
 
-            }
+                // }
 
         } catch (err) {
             console.log(err)
