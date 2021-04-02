d67999389e98621f6d9ed2d1d85caddf8a3ea0f1
Logan Leopold <loganleopld@me.com>
2021-04-02 14:50:57 -0400

    working through async object establishing

--- controllers/movieControl.js
+++ controllers/movieControl.js
@@ -1,3 +1,4 @@
+var mongoose = require('mongoose');
 const Movie = require("../models/movie")
 const Director = require("../models/director")
 const Platform = require("../models/platform")
@@ -24,12 +25,59 @@ module.exports = {
 
         try {   
 
+            /*
+
+            Movie to DB Pseudocode
+
+            2. Get values from req.body (above)
+            3. Establish IDs for use 
+                -explicit for movieID + directorID
+                -dynamic for actors + platforms
+            4. Establish arrays of objects writbale to MongoDB with new objectIds
+            
+            */ 
+           
+           // 1
+           let movieID = mongoose.Types.ObjectId()
+           let directorID = mongoose.Types.ObjectId()
+           let actorIDs = []
+           let platformIDs = []
+           
+           let insertDir = {
+               _id: directorID,
+               Name: director
+           }
+
+           
+           let actorsArr = actors.split(',').map( actor => {
+            // use this info for findoneandupdate with upsert - returned id will get pushed into actorIDs
+                // let actObj = {
+                //     Name: actor.trim(),
+                //     Movies: [movieID]
+                // }
+                // actObj.Name = actor.trim()
+                // actObj.Movies = [movieID]
+                // return actObj 
+                let actor = await Actor.findOneAndUpdate(
+                    {Name: actor.trim()}, 
+                    {$push: {Movies: movie._id}},
+                    {$upsert: true}
+                )
+
+                actorIDs.push(actor._id)
+           })
+
+            
+
+
+            // Below here is original try 
+
             //Create Movie
             var movie = null;
             let check = await Movie.findOne({Name: name})
 
             // Thinking about creating all of the document objects first so I have accurate IDs and then inserting them/creating them dynamically using objectIDs
-            
+
             if (!check) {
 
                 let genresInsert = genres.split(',').map( genre =>  genre.trim() )
@@ -57,7 +105,8 @@ module.exports = {
                     let actor = await Actor.findOneAndUpdate(
                         {Name: actor}, 
                         {$push: {Movies: movie._id}},
-                        {$upsert: true})
+                        {$upsert: true}
+                    )
                 })
 
 
