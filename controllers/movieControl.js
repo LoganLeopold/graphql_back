b325fe8d80c544d4a9f396eba3893c1497d918dc
Logan Leopold <loganleopld@me.com>
2021-05-05 20:29:16 -0400

    notes in movieControl.js and utilities for further work

--- controllers/movieControl.js
+++ controllers/movieControl.js
@@ -128,99 +128,59 @@ module.exports = {
 
         try {   
 
+            // let movieClear = await Movie.findByIdAndUpdate(
+            //     id, 
+            //     {
+            //         Name: '',
+            //         Director: null,
+            //         Actors: [],
+            //         Platforms: [],
+            //         TomatoPublic: null,
+            //         TomatoCritic: null,
+            //         Genres: []
+            //     },
+            //     {
+            //         new: true
+            //     }
+            // )
+            
             let newDocsArr = await auditDocs(id, req)
 
             console.log(newDocsArr)
 
-            if (1===0) {
-            let movieClear = await Movie.findByIdAndUpdate(
-                id, 
-                {
-                    Name: '',
-                    Director: null,
-                    Actors: [],
-                    Platforms: [],
-                    TomatoPublic: null,
-                    TomatoCritic: null,
-                    Genres: []
-                },
-                {
-                    new: true
-                }
-            )
-    
-            // let directorArr = await Promise.all(director.split(',').map(async (director) => {
-            //     let dir = await Director.findOneAndUpdate(
-            //         {Name: director.trim()},
-            //         {$addToSet: {'Movies': id}},
-            //         {upsert: true, new: true}
-            //     )
-            //     return dir._id
-            // }))
-            // let dir = await Director.findOneAndUpdate()
-
-            // THESE COULD MAYBE USE FINDONEANDUPDATE? 
-            // let actorsArr = await Promise.all(actor.split(',').map( async (act) => {
-            //     let actorIns = await Actor.findOneAndUpdate(
-            //         {Name: act.trim()}, 
-            //         {
-            //             $addToSet: {'Movies': id},
-            //             $setOnInsert: {
-            //                 Name: act.trim(),
-            //             }
-            //         },
-            //         {upsert: true, new: true}
-            //     )
-            //     return actorIns._id
-            // }))
-    
-            // let platformArr = await Promise.all(platform.split(',').map( async (plat) => {
-            //     let platformIns = await Platform.findOneAndUpdate(
-            //         {Name: plat.trim()},
-            //         {
-            //             $addToSet: {'Movies': id},
-            //             $setOnInsert: {
-            //                 Name: plat.trim(),
-            //             }
-            //         },
-            //         {upsert: true, new: true} 
-            //     )
-            //     return platformIns._id
-            // }))
-
+            /*
+            ------------------------------------------------------------------------------------
+            ------------------------------------------------------------------------------------
+            Now I must get the arrays from newDocsArr and assign them appropriately
+            ------------------------------------------------------------------------------------
+            ------------------------------------------------------------------------------------
+            */
             
-            
-            let newDocsArr = auditDocs(req, id, 'Movie')
-            
-            
-
             // let genreArr = genres.split(',').map( genre => {
             //     return genre.trim()
             // })
                 
-            movieClear.then( async () => {
+            // movieClear.then( async () => {
 
-                let movieNew = await Movie.findByIdAndUpdate(
-                    id, 
-                    {
-                        Name: name.trim(),
-                        Director: directorArr,
-                        Actors: actorsArr,
-                        Platforms: platformArr,
-                        TomatoPublic: tom_pub,
-                        TomatoCritic: tom_crit,
-                        Genres: genreArr
-                    },
-                    {
-                        new: true
-                    }
-                )
+            //     let movieNew = await Movie.findByIdAndUpdate(
+            //         id, 
+            //         {
+            //             Name: name.trim(),
+            //             Director: directorArr,
+            //             Actors: actorsArr,
+            //             Platforms: platformArr,
+            //             TomatoPublic: tom_pub,
+            //             TomatoCritic: tom_crit,
+            //             Genres: genreArr
+            //         },
+            //         {
+            //             new: true
+            //         }
+            //     )
                 
-                res.send(movieNew)
+            //     res.send(movieNew)
 
-            })
-
-        }
+            // })
 
         } catch (err) {
             console.log(err)
